3a781a4797b1212694fe4cdf922675017dbbe75a
pitta510 <pitta_510@hotmail.com>
2023-08-01 04:14:02 -0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Felipe Garcia on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
